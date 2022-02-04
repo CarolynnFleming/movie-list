70a792ffca5bc937e548b251dfea0731a3504821
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 15:41:16 -0500

    state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState('');
+  const [filter, setFilter] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
   return (
     <div className="App">
      
