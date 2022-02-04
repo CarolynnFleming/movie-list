0ccd737d3c921f2cf6c23f3eaf5faa1e4eba41ff
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 15:57:18 -0500

    object

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function MovieForm({ titleForm, setTitleForm, directorForm, setDirectorForm, yearForm, setYearForm, colorForm, setColorForm, addMovie }) {
+  function handleSubmit(e) {
+    const movie = {
+      title: titleForm,
+      director: directorForm,
+      year: yearForm,
+      color: colorForm,
+    };
+    
+  }
   return <div></div>;
 }
