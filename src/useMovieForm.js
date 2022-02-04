8305fa4612509dae16b0fd30b0d779abb96a510a
CarolynnFleming <cfcatpink@gmail.com>
2022-02-04 15:44:55 -0500

    set state

--- src/useMovieForm.js
+++ src/useMovieForm.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function useMovieForm() {
+    const [titleForm, setTitleForm] = useState('');
+    const [directorForm, setDirectorForm] = useState('');
+    const [yearForm, setYearForm] = useState('');
+    const [colorForm, setColorForm] = useState('red');
+
+    
   return <div></div>;
 }
